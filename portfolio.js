3d60865431930ba94289eae003c50692e1613fb8
francksefu <90141102+francksefu@users.noreply.github.com>
2023-01-31 01:46:58 +0200

    Add js fonctionnality by adding portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,30 @@
 const hamber = document.querySelector("#menu-clic");
 const menu = document.querySelector("#menu");
-const other = document.querySelector("#contain");
-const footer = document.querySelector("footer");
+const other = document.querySelectorAll(".hide");
+const x = document.querySelector("#x");
 
 function displ() {
   if(menu.style.display == "none"){
     menu.style.display = "block";
+    x.style.display = "block";
+    hamber.style.display = "none";
   }else {
     menu.style.display = "none";
+    x.style.display = "none";
+    hamber.style.display = "block";
   }
 }
 
 function hide() {
     menu.style.display = "none";
+    x.style.display = "none";
+    hamber.style.display = "block";
+}
+
+for(let i = 0; i < other.length; i++){
+  other[i].addEventListener("click", hide);
 }
 
 hamber.addEventListener("click", displ);
-other.addEventListener("click", hide);
-footer.addEventListener("click", hide);
+x.addEventListener("click", displ);
+
