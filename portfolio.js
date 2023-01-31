5ac8e85a54a4aa524868aab1a200e2bff1767a66
francksefu <90141102+francksefu@users.noreply.github.com>
2023-01-31 02:27:33 +0200

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,30 +1,29 @@
-const hamber = document.querySelector("#menu-clic");
-const menu = document.querySelector("#menu");
-const other = document.querySelectorAll(".hide");
-const x = document.querySelector("#x");
+const hamber = document.querySelector('#menu-clic');
+const menu = document.querySelector('#menu');
+const other = document.querySelectorAll('.hide');
+const x = document.querySelector('#x');
 
 function displ() {
-  if(menu.style.display == "none"){
-    menu.style.display = "block";
-    x.style.display = "block";
-    hamber.style.display = "none";
-  }else {
-    menu.style.display = "none";
-    x.style.display = "none";
-    hamber.style.display = "block";
+  if (menu.style.display === 'none') {
+    menu.style.display = 'block';
+    x.style.display = 'block';
+    hamber.style.display = 'none';
+  } else {
+    menu.style.display = 'none';
+    x.style.display = 'none';
+    hamber.style.display = 'block';
   }
 }
 
 function hide() {
-    menu.style.display = "none";
-    x.style.display = "none";
-    hamber.style.display = "block";
+  menu.style.display = 'none';
+  x.style.display = 'none';
+  hamber.style.display = 'block';
 }
 
 for(let i = 0; i < other.length; i++){
-  other[i].addEventListener("click", hide);
+  other[i].addEventListener('click', hide);
 }
 
-hamber.addEventListener("click", displ);
-x.addEventListener("click", displ);
-
+hamber.addEventListener('click', displ);
+x.addEventListener('click', displ);
