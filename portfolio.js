7e6bfa3329841d6ff69e7bf403b2f19a0fc0294f
francksefu <90141102+francksefu@users.noreply.github.com>
2023-01-31 02:50:00 +0200

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -21,7 +21,7 @@ function hide() {
   hamber.style.display = 'block';
 }
 
-for (let i = 0; i < other.length; i = i + 1) {
+for (let i = 0; i < other.length; i += 1) {
   other[i].addEventListener('click', hide);
 }
 
