bd0d23cfccffe062699722a5593ab271b5618f00
francksefu <90141102+francksefu@users.noreply.github.com>
2023-02-03 16:49:02 +0200

    Update form-validate.js

--- form-validate.js
+++ form-validate.js
@@ -1,5 +1,5 @@
-const form = document.querySelector('#form');
-const email = form.elements['email'];
+const form = document.forms[0];
+const email = form.elements["email"];
 const displayMessage = document.querySelector('#emailMessage');
 
 /**
@@ -7,25 +7,24 @@ const displayMessage = document.querySelector('#emailMessage');
  */
 
 function validateEmail() {
-    let reg = /[A-Z]/g;
-    if(reg.test(email.value)) {
-        email.style.borderColor = 'red';
-        displayMessage.innerText = 'remove all charactere in UPPERCASE please';
-        displayMessage.style.color = 'red';
-        return false;
-    } else {
-        email.style.border = 'none';
-        displayMessage.style.color = 'none';
-        displayMessage.innerText = '';
-        return true;
-    }
+  let reg = /[A-Z]/g;
+  if(reg.test(email.value)) {
+    email.style.borderColor = 'red';
+    displayMessage.innerText = 'remove all charactere in UPPERCASE please';
+    displayMessage.style.color = 'red';
+    return false;
+  }
+  if (!(reg.test(email.value))) {
+    email.style.border = 'none';
+    displayMessage.style.color = 'none';
+    displayMessage.innerText = '';
+    return true;
+  }
 }
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if(validateEmail()) {
-        form.submit();
-    }
+  e.preventDefault();
+  if (validateEmail()) {
+    form.submit();
+  }
 });
-
-
